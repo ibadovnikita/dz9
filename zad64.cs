841a2fe0bf238ec91f99b1d54d9892df3583fa98
IbadovNikita <blackcronos362@gmail.com>
2023-04-08 16:22:09 +0300

    zad64

---
+++ zad64.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введиете натуральное число ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Результат");
+if (N > 0)
+{
+    int natchisla (int N)
+    {   Console.WriteLine($"{N}");
+        if (N==1)
+        {
+            return 1;
+        }
+        return natchisla(N - 1);
+      
+    } 
+    natchisla(N);
+}
+else
+{
+  Console.Write("Не натуральное число");
+}
