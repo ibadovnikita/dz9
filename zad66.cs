34af476530771485ee84e68ba4d4ba2a5c52dc16
ibadovnikita <113236179+ibadovnikita@users.noreply.github.com>
2023-04-08 16:30:56 +0300

    Add files via upload

---
+++ zad66.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("Введиете число M");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введиете число N");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N>M)
+{
+    
+
+if(M>0 && N>0)
+{
+int sum(int M,int N)
+{
+   if (N == M)
+   {
+    return N;
+   }
+   return M + sum(M+1,N);
+}
+Console.WriteLine($"Результат {sum(M,N)}");
+}
+else
+ Console.Write("Не натуральное число ");
+}
+else
+Console.WriteLine("N должен быть больше M");
